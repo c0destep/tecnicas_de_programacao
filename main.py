52f503322d4d2f54f435aebaeef4aed7abcb837f
Lucas Alves <codestep@codingstep.com.br>
2022-04-30 22:13:47 -0300

    add array questions

--- main.py
+++ main.py
@@ -2,8 +2,43 @@ import statistics
 
 
 def welcome():
-    print('Welcome Developer :)')
-    item = int(input('Digite o número da Questão para acessa-lá: '))
+    print('Welcome Developer :)\n')
+    questions = [
+        "1. Elabore um algoritmo que imprima na tela lado a lado o texto 'Hello World!' 10 vezes.",
+        "2. Elabore um algoritmo que imprima na tela o texto 'Hello World!' 10 vezes, um por linha.",
+        "3. Elabore um algoritmo que imprima todos os números inteiros de 1 até 100 inclusive.",
+        "4. Elabore um algoritmo que imprima 100 vezes o texto '1- Hello World!' com o número.",
+        "5. Elabore um algoritmo que imprima todos os números decrescentes de 100 até 0 inclusive.",
+        "6. Elabore um algoritmo que imprima todos os números pares inteiros de 1 até 1000.",
+        "7. Elabore um algoritmo que imprima todos os números ímpares de 1000 até 0.",
+        "8. Elabore um algoritmo que imprima a soma dos 100 primeiros números inteiros positivos.",
+        "9. Elabore um algoritmo que solicite ao usuário um número inteiro que indicará a quantidade de vezes que o "
+        "texto 'Hello World!' será impresso na tela, um em cada linha.",
+        "10. Elabore um algoritmo que solicite ao usuário uma palavra e um número inteiro que indicará a quantidade "
+        "de vezes que a palavra digitada será impressa na tela, um em cada linha.",
+        "11. Elabore um algoritmo que leia um número de entrada que indicará a quantidade de números a serem lidos. "
+        "Em seguida, leia n números (conforme o valor informado anteriormente) e imprima a soma e a média aritmética "
+        "dos números informados.",
+        "12. Elabore um algoritmo que leia um número de entrada que indicará a quantidade de registros a serem lidos "
+        "(N). Em seguida algoritmo deve solicitar o nome e idade de N pessoas e ao final apresentar o nome da pessoa "
+        "mais velha.",
+        "13. Elabore um algoritmo que leia um número de entrada que indicará a quantidade de registros a serem lidos "
+        "(N). Em seguida algoritmo deve solicitar o sexo (M/F) e idade de N pessoas e ao final apresentar a média de "
+        "idade de ambos os gêneros catalogados.",
+        "14. Elabore um algoritmo que solicite ao usuário 10 números reais e ao final apresente o maior e o menor "
+        "deles.",
+        "15. Elabore um algoritmo que solicite N números reais e quando o usuário informar o valor nulo 0 (zero) o "
+        "programa ordene e mostre todos os números informados de forma crescente.",
+        "16. Escreva um programa que vá solicitando as idades dos alunos da sala até que todos sejam informados ("
+        "perguntar ao usuário se deseja informar a idade do próximo aluno). Ao final apresentar a idade do mais novo, "
+        "a idade do mais velho, Quantos alunos têm mais de 18 anos, quantos alunos têm até 18 anos, "
+        "a média aritmética e a mediana. "
+    ]
+
+    for q in questions:
+        print(q)
+
+    item = int(input('\nDigite o número da Questão para acessa-lá: '))
     return item
 
 
